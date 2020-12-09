6d551cfb895908604216bdf9373c497d7098eab9
unknown <groggydoggie@protonmail.com>
2020-12-09 00:44:01 +0000

    Added a powershell script

---
+++ Powershell/Transaction-Stats.ps1
@@ -0,0 +1,38 @@
+﻿param([string]$transactionFile)
+
+if(-not (Get-ChildItem $transactionFile))
+{
+    echo "Provide a transaction file you mug"
+}
+
+$transactionFile = "midata0086.csv"
+
+$transactionData = Import-Csv $transactionFile
+
+$transactionTypes = @{
+"ATM" = "Cash Machine Withdrawal"; 
+"BP" = "BACS or Bill Payment (including Bill Payment credit)"; 
+"CHQ" = "Cheque"; 
+"CIR"= "Cirrus or Maestro card transaction, or Eurocheque transaction";
+"CR" = "Credit (including BACS credit)";
+"DD" = "Direct Debit";
+"DIV" = "Dividend";
+"DR" = "Debit";
+"MAE" = "Maestro Debit Card Transaction";
+"SO" = "Standing Order";
+"TRF" = "Transfer";
+"VIS" = "Visa debit card payment";
+")))" = "Contactless debt card payment"}
+
+foreach($type in $transactionTypes.Keys)
+{
+    if(-not ($transactionData | Where {$_.Type -eq $type})){continue}
+
+    $stats = ($transactionData | Where {$_.Type -eq $type} | %{$_.'Debit/Credit' -replace "£", ""} | measure -Sum -Average -Maximum -Minimum)
+    echo "----------------------------------------"
+    echo "Type: $type. ($($transactionTypes[$type]))"
+    echo "Total: $($stats.Sum). Average: $($stats.Average). Min: $($stats.Max). Max: $($stats.Max)"
+
+}
+echo "----------------------------------------"
+
