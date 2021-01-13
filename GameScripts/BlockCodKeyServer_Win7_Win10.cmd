84669fd3ab58a6fae9f208093ea43709a5da2cea
vaalkor <groggydoggie@protonmail.com>
2021-01-13 00:58:34 +0000

    Added a bit of bootstrap stuff and some game scripts

---
+++ GameScripts/BlockCodKeyServer_Win7_Win10.cmd
@@ -0,0 +1,41 @@
+@echo off
+TITLE Block COD UO Activation Server
+color 0a
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+netsh advfirewall firewall show rule name="Block COD UO Activation" > nul
+if %ERRORLEVEL% == 0 goto :end
+
+netsh advfirewall firewall add rule name="Block COD UO Activation" protocol=any dir=out action=block remoteip=185.34.107.179 > nul
+
+:end
+netsh advfirewall firewall show rule name="Block COD UO Activation" 
+pause
