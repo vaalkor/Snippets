5c6269c2b4f96ac62c94f160535545d5c8baf021
vaalkor <groggydoggie@protonmail.com>
2021-01-23 18:25:44 +0000

    Added avp2 regkey script

---
+++ GameScripts/InstallAvpRegKeys.ps1
@@ -0,0 +1,11 @@
+﻿Set-Content -Path "C:\reee.txt" -Value "reee"
+$regFileContent = Get-Content "$PSScriptRoot\AVP.reg"
+$avpLocation = (ls $PSScriptRoot | ?{$_.Name -eq "Aliens vs. Predator 2 - Main Game"}).FullName.Replace('\', '\\')
+$avpHuntLocation = (ls $PSScriptRoot | ?{$_.Name -eq "Aliens versus Predator 2 - Primal Hunt"}).FullName.Replace('\', '\\')
+echo $avpLocation
+echo $avpHuntLocation
+$regFileContent = $regFileContent.Replace("<INSERT_AVP2_INSTALL_DIR>", $avpLocation).Replace("<INSERT_AVP2_PRIMALHUNT_INSTALL_DIR>", $avpHuntLocation)
+
+Set-Content -Path "$PSScriptRoot\AVP_Temp.reg" -Value $regFileContent
+
+&{reg import "$PSScriptRoot\AVP_Temp.reg" *>&1} | Out-NUll
