855ac30f84b9eda80a0545e2d2934357bda06af1
Robert Stamper <robert.stamper@ihsmarkit.com>
2021-08-12 12:48:16 +0100

    Update

--- Linux/bash.sh
+++ Linux/bash.sh
@@ -1,3 +1,9 @@
 quit_with_success_code() { exit 0; }
 trap quit_with_success_code ERR # Always exit with a success status
 
+find / -iname "blah"
+find / -type d
+du -sh dirname # Filesize for a directory
+
+
+
