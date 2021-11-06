9b13ee96fc9e3e23abddeaa35a40b275646a0e86
vaalkor <groggydoggie@protonmail.com>
2021-11-06 18:21:28 +0000

    Update

--- Linux/bash.sh
+++ Linux/bash.sh
@@ -5,5 +5,8 @@ find / -iname "blah"
 find / -type d
 du -sh dirname # Filesize for a directory
 
+# Absolutely awesome CLI text editor for windows n00bs like myself that can't be arsed to learn VIM and don't like nano.
+curl https://getmic.ro | bash && sudo mv micro /usr/bin
+micro myFile.txt
 
-
+ssh -i "KeyPairName.pem" username@servername.com
