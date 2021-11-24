610b16f1814179d3b187f75358068e47abbc06b0
Robert Stamper <robert.stamper@ihsmarkit.com>
2021-11-24 15:57:15 +0000

    Added funky powershell sorting trick

--- Powershell/tricks.ps1
+++ Powershell/tricks.ps1
@@ -1,2 +1,6 @@
 cat (Get-PSReadlineOption).HistorySavePath
 
+# Nifty way of reversing a stream of items. Uses funky scope parameter $script
+# https://stackoverflow.com/questions/22771724/reverse-elements-via-pipeline
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_scopes?view=powershell-7
+$thing | Sort-Object {(--$script:i)}
