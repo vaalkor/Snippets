4ab5d2a5fe9f44529188b081399feb30f847a14e
Robert Stamper <robert.stamper@ihsmarkit.com>
2021-11-29 15:16:32 +0000

    Update

--- Linux/bash.sh
+++ Linux/bash.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+#shellcheck disable=SC1091
+
 quit_with_success_code() { exit 0; }
 trap quit_with_success_code ERR # Always exit with a success status
 
@@ -15,3 +18,39 @@ find . -type f -exec zip my_zip {} +
 
 #print special characters. <<< is a here-string https://tldp.org/LDP/abs/html/x17837.html
 cat -etv <<<"$IFS"
+
+#Sourcing scripts into your current bash session. source and '.' are equivalent.
+source scripts/utils.sh
+. scripts/utils.sh
+
+#Getting directory name of path
+dirname scripts/crap/script1.sh # => scripts/crap
+
+# A demonstration of locally scoped variables! 
+declare -i count=0
+recurse_local()
+{
+  ((count++))
+  local x=$count
+  echo "x_before: $x"
+  if [ "$count" -eq 5 ]; then
+    echo "Done lad!"
+  else
+    recurse_local
+  fi
+  echo "x after: $x"
+}
+
+declare -i count=0
+recurse()
+{
+  ((count++))
+  x=$count
+  echo "x_before: $x"
+  if [ "$count" -eq 5 ]; then
+    echo "Done lad!"
+  else
+    recurse
+  fi
+  echo "x after: $x"
+}
